9d00631322e7fe9a3b35a4a7efca14c958005381
Łukasz Michalak <lukasz.michalak96@outlook.com>
2021-02-05 13:16:10 +0100

    Add form in view of home, Add function of login, Add success view, Remove example views

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-import { route } from './router';
+import { route } from "./router";
+import { API_URL } from "./utils";
 
-route('/', 'home', function() {
-  this.where = 'here';
-});
+route("/", "home", function () {
+  this.message = "";
+  this.$on(".login-form", "submit", (e) => {
+    e.preventDefault();
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
-});
+    const userDataForm = new FormData(e.currentTarget);
 
-route('/ex2', 'example2', function() {
-  this.title = 'Example 2';
-  this.counter = 0;
-  this.$on('.my-button', 'click', () => {
-    this.counter += 1;
-    this.$refresh();
+    const username = userDataForm.get("username");
+    const password = userDataForm.get("password");
+
+    const userData = { username, password };
+
+    const logIn = async () => {
+      const response = await fetch(`${API_URL}/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        this.message = "Bad login or password!";
+        this.$refresh();
+        throw new Error(message);
+      }
+
+      window.location.replace("#/success");
+    };
+
+    logIn();
   });
 });
 
-route('*', '404', function () {});
+route("/success", "success", function () {
+  this.title = `You're log in!`;
+});
+
+route("*", "404", function () {});
---
+++ utils.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://zwzt-zadanie.netlify.app";
